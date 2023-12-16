b3a523700f96cc4deaab7a898ac4f8e74165de1d
Varshini Naguleswaran <varshininaguleswaran@gmail.com>
2023-12-16 20:39:24 +0530

    verify

--- childgrowthindicator/src/component/VerifyPass.js
+++ childgrowthindicator/src/component/VerifyPass.js
@@ -1,6 +1,11 @@
 export default function VerifyPass()
 {
     return(
-        <></>
+        <>
+        <div>
+            <div class="navBar"></div>
+            <div class="verifyBody"></div>
+        </div>
+        </>
     );
 }
