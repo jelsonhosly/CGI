7d004054b72d665c10bcd72bce86ab3a7ed11556
L A D N Thathsara <141746388+Thathsara1999@users.noreply.github.com>
2024-05-15 17:09:19 +0530

    Update SignupForm.js

--- src/component/SignupForm.js
+++ src/component/SignupForm.js
@@ -2,3 +2,9 @@ import React, { useState } from 'react';
 import { CardImg, Row,Col, Button, Card, CardBody, CardTitle, Form, FormGroup, Label, Input, Container } from 'reactstrap';
 import axios from 'axios';
 import heroImage from '../assets/images/logo.png'; 
+
+const SignupForm = ({ onSignupSuccess, nextChildID }) => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [childID, setChildID] = useState(nextChildID);
