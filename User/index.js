2d689bbb185f4154eb8ad06963acc98fe7f661c8
jelsonhosly <87078456+jelsonhosly@users.noreply.github.com>
2024-05-15 21:16:43 +0530

    Update index.js

--- User/index.js
+++ User/index.js
@@ -3,7 +3,6 @@ import {createRoot} from 'react-dom/client';
 import "./assets/scss/style.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
 import Loader from "./layouts/loader/Loader";
 
 const rootElement = document.getElementById('root');
@@ -11,11 +10,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <Suspense fallback={<Loader />}>
-    <HashRouter>
-      <App />
-    </HashRouter>
+      <App/>
   </Suspense>,
 );
-
 
 reportWebVitals();
