5ec585a5c32aed595f283978ba53e083eb457e2b
L A D N Thathsara <141746388+Thathsara1999@users.noreply.github.com>
2024-05-15 17:09:43 +0530

    Update SignupForm.js

--- src/component/SignupForm.js
+++ src/component/SignupForm.js
@@ -8,3 +8,23 @@ const SignupForm = ({ onSignupSuccess, nextChildID }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [childID, setChildID] = useState(nextChildID);
+
+  const handleSignup = async () => {
+    try {
+      const response = await axios.post('http://localhost:5050/signup', {
+        email,
+        username,
+        password,
+        childID: nextChildID 
+      });
+
+      if (response.data.message === 'User created successfully') {
+        onSignupSuccess(); 
+      } else {
+        // Handle signup failure
+      }
+    } catch (error) {
+      console.error('Signup failed:', error);
+      // Handle signup failure
+    }
+  };
